e35230c4730ac7b3f28db9dd589d575b85302f1d
pashtetrus33@ <pashtetrus@gmail.com>
2022-08-14 13:08:44 +0300

    task for lesson 3

---
+++ lesson_3/task_from_lection/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+int[] arr = {1,4,5,6,7,8,9,1,1,3,4,5,7};
+
+void printArray (int[] array)
+{
+for (int i = 0; i < array.Length; i++)
+Console.Write($"{array[i]} ");
+Console.WriteLine();
+Console.WriteLine(array.Length);
+}
+
+void sortArray (int[] array)
+{
+for (int i = 0; i < array.Length-1; i++)
+    {
+    int maxPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+            {
+                if (array[j] > array[maxPosition])
+                maxPosition = j;
+            }   
+        int temprory = array[i];
+            array[i] = array[maxPosition];
+            array[maxPosition] = temprory;
+    }  
+}
+
+printArray(arr);
+sortArray(arr);
+printArray(arr);
+
---
+++ lesson_4/1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти: ");
+int quaterNumber = Convert.ToInt32(Console.ReadLine());
+
+if ((quaterNumber < 1) || (quaterNumber > 4))
+{
+    Console.WriteLine("Ввели неправильный номер четверти (от 1 до 4) ");
+
+}
+
+else if (quaterNumber == 1)
+{
+Console.WriteLine("Диапазон  0 < x < + inf, 0 < y < + inf");
+}
+
+else if (quaterNumber == 2)
+{
+Console.WriteLine("Диапазон - inf < x < 0, 0 < y < + inf");
+}
+
+else if (quaterNumber == 3)
+{
+Console.WriteLine("Диапазон - inf < x < 0, - inf < y < 0");
+}
+else
+{
+Console.WriteLine("Диапазон 0 < x < + inf, - inf < y < 0");
+}
