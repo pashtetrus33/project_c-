3e2c42f51c64d5315567019c732f8e8713e6ab1e
pashtetrus33@ <pashtetrus@gmail.com>
2022-08-17 09:45:31 +0300

    dz_5_2

--- dz_5/dz_5_2/Program.cs
+++ dz_5/dz_5_2/Program.cs
@@ -1,31 +1,68 @@
-﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
 
-452 -> 11
-82 -> 10
-9012 -> 12*/
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
 
 Console.Clear();
-Console.WriteLine("Программа принимает на вход число и выдаёт сумму цифр в числе.");
+Console.WriteLine("Программа находит сумму элементов, стоящих на нечётных позициях.");
 bool repeat = true;
 
 while (repeat)
 {
-int answer = 0;
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(Console.ReadLine());
-int temp = number;
-if (temp < 0)
-temp = - temp;
-while (temp > 0)
+Console.Write("Введите раземер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+if (arraySize < 1)
 {
-    answer += temp % 10;
-    temp /= 10;
+    Console.WriteLine("Размер массива должен быть больше 0, попробуйте снова!");
+    continue;
 }
 
-Console.WriteLine($"Cумма цифр числа {number} равна {answer}");
+Console.Write("Введите минимальное значение элемента массива: ");
+int minRandom = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите максимальное значение элемента массива: ");
+int maxRandom = Convert.ToInt32(Console.ReadLine());
+if (minRandom > maxRandom)
+{
+    Console.WriteLine("Неверный ввод: максимальное значение меньше минимального!");
+    continue;
+}
+//Метод создания массива с указанным размером (size) и указанными границами (minRandom, maxRandom) , заполненого случайными числами.
+int[] createRandomArray(int size, int minRandom, int maxRandom)
+{
+    int[] arr = new int[size];
+    for (int i=0; i < size; i++)
+    arr[i] = new Random().Next(minRandom,maxRandom + 1);
+    return arr;
+}
+
+//Метод печати массива в консоль
+void printArray(int[] arr)
+{
+    for (int i=0; i < arr.Length; i++)
+    Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+//Метод подсчета суммы элементов стоящих на нечетных позициях в массиве
+int sumOddNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i=1; i < array.Length; i+=2)
+    {
+    sum +=array[i];  
+    }
+return sum;
+}
+
+int[] randomArray = createRandomArray(arraySize, minRandom, maxRandom);
+printArray(randomArray);
+int answer = sumOddNumbers(randomArray);
+Console.WriteLine($"Cумма чисел, стоящих на нечетнх позициях, равна {answer}");
+
 Console.WriteLine("Для продолжения нажмите 'ENTER', для выхода 'Q' затем 'ENTER'");
 string? decision = Console.ReadLine();
-
-if (decision?.ToLower() == "q")
+if ((decision?.ToLower() == "q") || (decision?.ToLower() == "й"))
     repeat = false;
 }
--- dz_5/dz_5_3/Program.cs
+++ dz_5/dz_5_3/Program.cs
@@ -1,58 +1,30 @@
-﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
 
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-6, 1, 33 -> [6, 1, 33]*/
+[3 7 22 2 78] -> 76*/
 
 Console.Clear();
-Console.WriteLine("Программа задаёт массив из 8 элементов и выводит их на экран.");
+Console.WriteLine("Программа находит разницу между максимальным и минимальным элементов массива с вещественными числами.");
 bool repeat = true;
-int CONST = 8;  // Константа для задания размера массива
-
-// Метод заполнения массива
-int[]  arrayFill(int count, int min, int max)
-{
-int[] temp = new int[count];   
-int i = 0;    
-while (i < temp.Length)
-{
-temp[i] = new Random().Next(min, max+1);
-i++;
-}
-
-return temp;
-}
-
-// Метод вывода массива в консль
-void arrayPrint(int[] temp)
-{   
-int i = 0;    
-while (i < temp.Length)
-{
-Console.Write($"[{temp[i]}] ");
-i++;
-}
-}
 
 while (repeat)
 {
-Console.Write("Введите минимальное значение элемента массива: ");
-int minRandom = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите максимальное значение элемента массива: ");
-int maxRandom = Convert.ToInt32(Console.ReadLine());
-if (minRandom > maxRandom)
+int answer = 0;
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int temp = number;
+if (temp < 0)
+temp = - temp;
+while (temp > 0)
 {
-    Console.WriteLine("Неверный ввод: максимальное значение меньше минимального!");
-    continue;
+    answer += temp % 10;
+    temp /= 10;
 }
 
-int [] array = arrayFill(CONST, minRandom, maxRandom);
-arrayPrint(array);
-Console.WriteLine();
+Console.WriteLine($"Cумма цифр числа {number} равна {answer}");
 Console.WriteLine("Для продолжения нажмите 'ENTER', для выхода 'Q' затем 'ENTER'");
 string? decision = Console.ReadLine();
 
 if (decision?.ToLower() == "q")
-    {
-        repeat = false;
-    }
+    repeat = false;
 }
