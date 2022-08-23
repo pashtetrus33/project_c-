1864fe18cfc958a99dabce2d75d9e237d6b44a07
pashtetrus33@ <pashtetrus@gmail.com>
2022-08-23 22:13:59 +0300

    line print method for console

--- dz_6/dz_6_2/Program.cs
+++ dz_6/dz_6_2/Program.cs
@@ -1,6 +1,5 @@
 ﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-
 k1 = 5, b1 = 2, k2 = 9, b2 = 4 -> (-0,5; -0,5) */
 
 bool repeat = true;
@@ -22,10 +21,43 @@ while (repeat)
     Console.Write("Введите b2: ");
     double b2 = Convert.ToDouble(Console.ReadLine());
 
+    // Попытка cоздания графического вывода линий в консоль
+    void pritnLine(int a1, int b1, int a2, int b2)
+    {
+        // Задаем двумерный массив для вывода
+        int fieldYSize = 61;
+        int fieldXSize = 61;
+        string[,] field = new string[fieldYSize, fieldXSize];
+
+        for (int i = 0; i < field.GetLength(0); i++)
+        {
+            //string xLine = (field.GetLength(0) / 2 - i).ToString();
+            //Console.Write(xLine.PadLeft(3));
+            for (int j = 0; j < field.GetLength(1); j++)
+            {
+                if ((i == field.GetLength(0) / 2 + a1 * (field.GetLength(1) / 2 - j) - b1) || (i == field.GetLength(0) / 2 + a2 * (field.GetLength(1) / 2 - j) - b2))
+                    field[i, j] = "*";
+                else
+                    field[i, j] = " ";
+                Console.Write($"{field[i, j]} ");
+
+                if (j == field.GetLength(1) / 2)
+                {
+                    string xLine = (field.GetLength(0) / 2 - i).ToString();
+                    Console.Write(xLine.PadLeft(3));
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    //Попытка вывода линий на экране в виде звездочек
+    pritnLine((int)k1, (int)b1, (int)k2, (int)b2);
+
     // Проверка на параллельность прямых
     if ((k1 == k2) && (b1 != b2))
     {
-        Console.WriteLine("Прямые - параллельные!");
+        Console.WriteLine("Прямые - параллельные. Точки пересечения нет!");
         Console.WriteLine();
         Console.WriteLine("Для продолжения нажмите 'ENTER', для выхода 'Q' затем 'ENTER'");
         if (Console.ReadLine()?.ToLower() == "q")
